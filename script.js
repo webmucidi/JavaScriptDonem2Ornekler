6ba4963bc623b17d32c0361b2ffe6c431b47f926
webmucidi <webmucidicom@gmail.com>
2021-02-25 18:13:53 +0300

    25.02.2021

--- script.js
+++ script.js
@@ -63,3 +63,121 @@ function merhaba(){
         document.write("Hoşgeldiniz..."+"<br>");
     }
 }
+
+
+function hesapla(){
+    //Değişkenleri tanımlama adımı
+    let sinav,performans,ortalama;
+
+    //Değişkenlere değer aktarma adımı(Alınan değeri sayısala dönüştürdük!)
+    sinav=Number(document.getElementById("txtSinav").value);
+    performans=Number(document.getElementById("txtPerformans").value);
+
+    //Hesaplama adımı
+    ortalama=(sinav+performans)/2;
+
+    //Sonucu yazdırmadan önce şart kontrolü ve sonucu yazdırma adımı
+    if(ortalama<50)
+    {
+        document.getElementById("sonuc").innerHTML="Not ortalamanız: " + ortalama +"<br>"+ "Maalesef KALDINIZ!";
+    }
+    else
+    {
+        document.getElementById("sonuc").innerHTML="Not ortalamanız: " + ortalama +"<br>"+ "Tebrikler GEÇTİNİZ";
+
+    }
+}
+
+function dortislem(){
+
+    //Değişkenleri girdiler ve çıktılar için tanımlama adımı
+    let secim,sayi1,sayi2,sonuc;
+
+    //secim değişkeni içerisine listede gezerek seçili olan değeri aktarma
+    for(let i=0;i<document.getElementById("islemler").length;i++)
+    {
+        if(document.getElementById("islemler").options[i].selected)
+        {
+            secim=document.getElementById("islemler").options[i].value;
+        }   
+    }
+
+    //Text kutularına girilen değerleri sayi1 ve sayi2 değişkenlerine aktarma
+    sayi1=Number(document.getElementById("txtSayi1").value);
+    sayi2=Number(document.getElementById("txtSayi2").value);
+
+    //İşlem seçimine göre şart kontrollü hesaplama yapma adımı
+    if(secim=="+")
+    {
+        sonuc=sayi1+sayi2;
+        document.getElementById("sonuc").innerHTML=sonuc;
+    }
+    else if(secim=="-")
+    {
+        sonuc=sayi1-sayi2;
+        document.getElementById("sonuc").innerHTML=sonuc;
+    }
+    else if(secim=="*")
+    {
+        sonuc=sayi1*sayi2;
+        document.getElementById("sonuc").innerHTML=sonuc;
+    }
+    else if(secim=="/")
+    {
+        sonuc=sayi1/sayi2;
+        document.getElementById("sonuc").innerHTML=sonuc;
+    }
+    else if(secim=="#")
+    {    
+        document.getElementById("sonuc").innerHTML="Seçim yapmadınız!";
+    }
+
+}
+
+function cevir(){
+    let para,secim,sonuc;
+    const dolar=0.13,euro=0.10;
+
+    para=Number(document.getElementById("txtPara").value);
+
+    for(let i=0;i<document.getElementById("slctDoviz").length;i++)
+    {
+        if(document.getElementById("slctDoviz").options[i].selected)
+        {
+            secim=document.getElementById("slctDoviz").options[i].value;
+        }  
+
+        //console.log(secim);
+    }
+
+    if(secim=="$")
+    {
+        sonuc=para*dolar;
+        document.getElementById("sonuc").innerHTML="Paranızın seçtiğiniz döviz cinsinden karşılığı: "+sonuc+secim;
+
+    }
+    else if(secim=="E")
+    {
+        sonuc=para*euro;
+        document.getElementById("sonuc").innerHTML="Paranızın seçtiğiniz döviz cinsinden karşılığı: "+sonuc+secim;
+
+    }
+    else if(secim=="#")
+    {
+        
+        document.getElementById("sonuc").innerHTML="Seçim yapmadınız!!!";
+    }
+
+
+}
+
+function taksimetre(){
+    let mesafe,odenecekTutar;
+    const tarife=3.5;
+
+    mesafe=Number(document.querySelector("#txtMesafe").value);
+
+    odenecekTutar=mesafe*tarife;
+
+    document.querySelectorAll("h3")[0].innerHTML="Gideceğiniz mesafe için ortalama ücret: "+odenecekTutar+" TL'dir";
+}
